32058d827aad524ea9949f59d00bdf48bb4d299c
Dennis Tang <dennis.sht@gmail.com>
2018-11-15 20:27:41 +0100

    Implemented the  aircraft data class to hold all properties of an aircraft.

--- BalancedFieldLength/Calculator/test/Calculator.Data.Test/AircraftDataTest.cs
+++ BalancedFieldLength/Calculator/test/Calculator.Data.Test/AircraftDataTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Calculator.Data.TestUtil;
 using NUnit.Framework;
 
 namespace Calculator.Data.Test
@@ -7,22 +8,53 @@ namespace Calculator.Data.Test
     public class AircraftDataTest
     {
         [Test]
+        public static void Constructor_AerodynamicDataNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+
+            // Call 
+            TestDelegate call = () => new AircraftData(random.Next(), random.NextDouble(),
+                                                       random.NextDouble(), random.NextDouble(), 
+                                                       random.NextDouble(), random.NextDouble(), 
+                                                       random.NextDouble(), null);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("aerodynamicData", exception.ParamName);
+        }
+
+        [Test]
         public static void Constructor_ExpectedValues()
         {
             // Setup
             var random = new Random(21);
             int nrOfEngines = random.Next();
-            double aspectRatio = random.NextDouble();
-            double wingArea = random.NextDouble();
+            double maximumThrustPerEngine = random.NextDouble();
             double takeOffWeight = random.NextDouble();
-            double maximumThrust = random.NextDouble();
+            double pitchAngleGradient = random.NextDouble();
+            double maximumPitchAngle = random.NextDouble();
+            double rollingResistanceCoefficient = random.NextDouble();
+            double brakingResistanceCoefficient = random.NextDouble();
 
-            
+            AerodynamicData aerodynamicData = AerodynamicDataTestFactory.CreateAerodynamicData();
+
             // Call
-            var aircraftData = new AircraftData();
+            var aircraftData = new AircraftData(nrOfEngines, maximumThrustPerEngine,
+                                                takeOffWeight,
+                                                pitchAngleGradient, maximumPitchAngle,
+                                                rollingResistanceCoefficient, brakingResistanceCoefficient,
+                                                aerodynamicData);
 
             // Assert
-            
+            Assert.AreEqual(nrOfEngines, aircraftData.NrOfEngines);
+            Assert.AreEqual(maximumThrustPerEngine, aircraftData.MaximumThrustPerEngine);
+            Assert.AreEqual(takeOffWeight, aircraftData.TakeOffWeight);
+            Assert.AreEqual(pitchAngleGradient, aircraftData.PitchAngleGradient);
+            Assert.AreEqual(maximumPitchAngle, aircraftData.MaximumPitchAngle);
+            Assert.AreEqual(rollingResistanceCoefficient, aircraftData.RollingResistanceCoefficient);
+            Assert.AreEqual(brakingResistanceCoefficient, aircraftData.BrakingResistanceCoefficient);
+            Assert.AreSame(aerodynamicData, aircraftData.AerodynamicData);
         }
     }
 }
