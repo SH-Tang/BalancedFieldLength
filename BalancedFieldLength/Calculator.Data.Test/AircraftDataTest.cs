1694902a5629e2a8331783497562a0abcaf4af9e
Dennis Tang <dennis.sht@gmail.com>
2018-11-15 19:02:31 +0100

    Added data classes to contain the aircraft data and properties

---
+++ BalancedFieldLength/Calculator.Data.Test/AircraftDataTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Calculator.Data.Test
+{
+    [TestFixture]
+    public class AircraftDataTest
+    {
+        [Test]
+        public static void Constructor_ExpectedValues()
+        {
+            // Setup
+            var random = new Random(21);
+            int nrOfEngines = random.Next();
+            double aspectRatio = random.NextDouble();
+            double wingArea = random.NextDouble();
+            double takeOffWeight = random.NextDouble();
+            double maximumThrust = random.NextDouble();
+
+            
+            // Call
+            var aircraftData = new AircraftData();
+
+            // Assert
+            
+        }
+    }
+}
