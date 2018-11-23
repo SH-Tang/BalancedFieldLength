fedba0eb735215929d41c24eeb04d6adce2c1c50
Dennis Tang <dennis.sht@gmail.com>
2018-11-23 21:03:47 +0100

    Introduced aircraft testdata
    - Added test cases representing actual aircraft

--- BalancedFieldLength/Simulator/Simulation/test/Simulator.Data.TestUtil/AircraftTestData.cs
+++ BalancedFieldLength/Simulator/Simulation/test/Simulator.Data.TestUtil/AircraftTestData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Calculator.Data;
 using NUnit.Framework;
 
 namespace Simulator.Data.TestUtil
@@ -10,7 +11,33 @@ namespace Simulator.Data.TestUtil
     public static class AircraftTestData
     {
         /// <summary>
-        /// Gets the test cases containing aerodynamic data.
+        /// Gets the test cases containing real aircraft data.
+        /// </summary>
+        /// <returns>The test cases with actual aircraft data.</returns>
+        /// <remarks>This test case data can be used as:
+        /// <code>
+        /// [Test]
+        /// [TestCaseSource(typeof(AircraftTestData), nameof(AircraftTestData.GetAircraftData))]
+        /// public static void SomeTest(AircraftData aircraftData)
+        /// {
+        ///     // Test
+        /// }
+        /// </code>
+        /// </remarks>
+        public static IEnumerable<TestCaseData> GetAircraftData()
+        {
+            TestAerodynamicData[] aerodynamicTestData = GetAerodynamicData();
+            yield return new TestCaseData(new AircraftData(2, 75, 500, 6, 16, 0.02, 0.2, aerodynamicTestData[0].Data))
+                .SetName(aerodynamicTestData[0].TestName);
+            yield return new TestCaseData(new AircraftData(3, 120, 1200, 5, 15, 0.02, 0.2, aerodynamicTestData[1].Data))
+                .SetName(aerodynamicTestData[1].TestName);
+            yield return new TestCaseData(new AircraftData(4, 300, 3500, 4, 14, 0.02, 0.2, aerodynamicTestData[2].Data))
+                .SetName(aerodynamicTestData[2].TestName);
+
+        }
+
+        /// <summary>
+        /// Gets the test cases containing real aerodynamic data.
         /// </summary>
         /// <returns>The test cases with aerodynamic data.</returns>
         /// <remarks>This test case data can be used as:
@@ -29,11 +56,14 @@ namespace Simulator.Data.TestUtil
                                                    .SetName(testData.TestName));
         }
 
-        private static IEnumerable<TestAerodynamicData> GetAerodynamicData()
+        private static TestAerodynamicData[] GetAerodynamicData()
         {
-            yield return new TestAerodynamicData(new AerodynamicData(15, 100, -3, 4.85, 1.60, 0.021, 0.026, 0.85), "Two Jet Power Engine");
-            yield return new TestAerodynamicData(new AerodynamicData(14, 200, -4, 4.32, 1.45, 0.024, 0.028, 0.80), "Three Jet Power Engine");
-            yield return new TestAerodynamicData(new AerodynamicData(12, 500, -5, 3.95, 1.40, 0.026, 0.029, 0.82), "Four Jet Power Engine");
+            return new[]
+                   {
+                       new TestAerodynamicData(new AerodynamicData(15, 100, -3, 4.85, 1.60, 0.021, 0.026, 0.85), "Two Jet Power Engine"),
+                       new TestAerodynamicData(new AerodynamicData(14, 200, -4, 4.32, 1.45, 0.024, 0.028, 0.80), "Three Jet Power Engine"),
+                       new TestAerodynamicData(new AerodynamicData(12, 500, -5, 3.95, 1.40, 0.026, 0.029, 0.82), "Four Jet Power Engine")
+                   };
         }
 
         private class TestAerodynamicData
