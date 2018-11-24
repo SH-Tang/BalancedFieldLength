10536cf8d7af219a4027b17ec1a66015082c576f
Dennis Tang <dennis.sht@gmail.com>
2018-11-24 18:09:23 +0100

    Added angle struct
    - Used to represent an angle in different formats.

---
+++ BalancedFieldLength/Simulator/Core/src/Core.Common.Data/Angle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Core.Common.Data
+{
+    /// <summary>
+    /// Represents an angle between 0 and 360 degrees.
+    /// </summary>
+    public struct Angle
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="Angle"/>
+        /// with a given angle.
+        /// </summary>
+        /// <param name="angle">The angle in degrees.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when
+        /// <paramref name="angle"/> is not between [0, 360] degrees.</exception>
+        public Angle(double angle)
+        {
+            if (angle < 0 || angle > 360)
+            {
+                throw new ArgumentOutOfRangeException(nameof(angle),
+                                                      "Invalid angle, angle must be in the range of [0, 360]");
+            }
+
+            Degrees = angle;
+            Radians = (Degrees * Math.PI) / 180;
+        }
+
+        public double Degrees { get; }
+        public double Radians { get; }
+    }
+}
---
+++ BalancedFieldLength/Simulator/Core/test/Core.Common.Data.Test/AngleTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Core.Common.Data.Test
+{
+    [TestFixture]
+    public class AngleTest
+    {
+        private const double tolerance = 10e-6;
+
+        [Test]
+        [TestCase(0 - tolerance)]
+        [TestCase(360 + tolerance)]
+        public static void Constructor_InvalidAngle_ThrowsArgumentOutOfRangeException(double degrees)
+        {
+            // Call 
+            TestDelegate call = () => new Angle(degrees);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(call);
+            const string expectedMessage = "Invalid angle, angle must be in the range of [0, 360]";
+            string message = exception.Message.Split(new[]
+                                                     {
+                                                         Environment.NewLine
+                                                     }, StringSplitOptions.None).First();
+            Assert.AreEqual(expectedMessage, message);
+        }
+
+        [Test]
+        public static void Constructor_WithoutArguments_ExpectedValues()
+        {
+            // Call 
+            var angle = new Angle();
+
+            // Assert
+            Assert.Zero(angle.Degrees);
+            Assert.Zero(angle.Radians);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(180)]
+        [TestCase(360)]
+        [TestCase(double.NaN)]
+        public static void GivenAngleWithValidValues_WhenCallingAsDegrees_ReturnsAngleInDegrees(double degrees)
+        {
+            // Given
+            var angle = new Angle(degrees);
+
+            // When 
+            double angleInDegrees = angle.Degrees;
+
+            // Then
+            Assert.AreEqual(degrees, angleInDegrees);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(180)]
+        [TestCase(360)]
+        [TestCase(double.NaN)]
+        public static void GivenAngleWithValidValues_WhenCallingAsRadians_ThenReturnsAngleInRadians(double degrees)
+        {
+            // Given
+            var angle = new Angle(degrees);
+
+            // When 
+            double angleInRadians = angle.Radians;
+
+            // Then
+            Assert.AreEqual(DegreesToRadians(degrees), angleInRadians, tolerance);
+        }
+
+        private static double DegreesToRadians(double degrees)
+        {
+            return (degrees * Math.PI) / 180;
+        }
+    }
+}
