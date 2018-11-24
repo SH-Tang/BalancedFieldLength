f68032377f277060f7a36cbdb024242a05221f62
Dennis Tang <dennis.sht@gmail.com>
2018-11-24 18:16:54 +0100

    Added test helper file containing simulation constants
    - Introduced for reusability and as a preparation for the other calculators.

---
+++ BalancedFieldLength/Simulator/Simulation/test/Simulator.Data.TestUtil.Test/SimulationConstantsTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Simulator.Data.TestUtil.Test
+{
+    [TestFixture]
+    public class SimulationConstantsTest
+    {
+        [Test]
+        public static void SimulationConstants_Always_ReturnsExpectedValues()
+        {
+            // Assert
+            Assert.AreEqual(10e-6, SimulationConstants.Tolerance);
+            Assert.AreEqual(9.81, SimulationConstants.GravitationalAcceleration);
+            Assert.AreEqual(1.225, SimulationConstants.Density);
+        }
+    }
+}
---
+++ BalancedFieldLength/Simulator/Simulation/test/Simulator.Data.TestUtil/SimulationConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace Simulator.Data.TestUtil
+{
+    /// <summary>
+    /// Class which contains constants which can be used for testing.
+    /// </summary>
+    public static class SimulationConstants
+    {
+        /// <summary>
+        /// Represents the tolerance of double deviations due to rounding.
+        /// </summary>
+        public const double Tolerance = 10e-6;
+
+        /// <summary>
+        /// Represents the gravitational acceleration. m/s^2
+        /// </summary>
+        public const double GravitationalAcceleration = 9.81;
+
+        /// <summary>
+        /// Represents the air density. [kg/m^3] 
+        /// </summary>
+        public const double Density = 1.225;
+    }
+}
