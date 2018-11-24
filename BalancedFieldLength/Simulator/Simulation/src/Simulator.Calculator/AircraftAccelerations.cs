d9aa2f33afd16612cc7b5bf2db96a1c1da82b915
Dennis Tang <dennis.sht@gmail.com>
2018-11-24 17:00:08 +0100

    Moved private calculator classes to their respective project

---
+++ BalancedFieldLength/Simulator/Simulation/src/Simulator.Calculator/AircraftAccelerations.cs
@@ -0,0 +1,31 @@
+﻿namespace Simulator.Calculator {
+    /// <summary>
+    /// Class which contains the time derivatives of the aircraft states.
+    /// </summary>
+    public class AircraftAccelerations
+    {
+        /// <summary>
+        /// Gets the pitch rate. [deg/s]
+        /// </summary>
+        /// <remarks>Also denoted as dTheta/dt.</remarks>
+        public double PitchRate { get; set; }
+
+        /// <summary>
+        /// Gets the climb rate. [m/s]
+        /// </summary>
+        /// <remarks>Also denoted as dh/dt.</remarks>
+        public double ClimbRate { get; set; }
+
+        /// <summary>
+        /// Gets the true airspeed rate. [m/s^2]
+        /// </summary>
+        /// <remarks>Also denoted as dVtas/dt.</remarks>
+        public double TrueAirSpeedRate { get; set; }
+
+        /// <summary>
+        /// Gets the flight path angle rate. [rad/s]
+        /// </summary>
+        /// <remarks>Also  denoted as dGamma/dt.</remarks>
+        public double FlightPathRate { get; set; }
+    }
+}
---
+++ BalancedFieldLength/Simulator/Simulation/src/Simulator.Calculator/AircraftState.cs
@@ -0,0 +1,46 @@
+﻿namespace Simulator.Calculator {
+    /// <summary>
+    /// Class which contains the aircraft states.
+    /// </summary>
+    public class AircraftState
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="AircraftState"/>.
+        /// </summary>
+        /// <param name="pitchAngle">The pitch angle. [deg]</param>
+        /// <param name="flightPathAngle">The flight path angle. [deg]</param>
+        /// <param name="trueAirspeed">The true airspeed. [m/s]</param>
+        /// <param name="height">The height. [m]</param>
+        public AircraftState(double pitchAngle, double flightPathAngle, double trueAirspeed, double height)
+        {
+            PitchAngle = pitchAngle;
+            FlightPathAngle = flightPathAngle;
+            TrueAirspeed = trueAirspeed;
+            Height = height;
+        }
+
+        /// <summary>
+        /// Gets the pitch angle. [deg]
+        /// </summary>
+        /// <remarks>Also denoted as theta.</remarks>
+        public double PitchAngle { get; }
+
+        /// <summary>
+        /// Gets the flight path angle. [deg]
+        /// </summary>
+        /// <remarks>Also denoted as gamma.</remarks>
+        public double FlightPathAngle { get; }
+
+        /// <summary>
+        /// Gets the true airspeed. [m/s]
+        /// </summary>
+        /// <remarks>Also denoted as Vtas.</remarks>
+        public double TrueAirspeed { get; }
+
+        /// <summary>
+        /// Gets the height. [m]
+        /// </summary>
+        /// <remarks>Also denoted as h.</remarks>
+        public double Height { get; }
+    }
+}
