d20c54daaaa9975f80a3b526b7f99ac4ac7bdff7
Dennis Tang <dennis.sht@gmail.com>
2018-11-25 15:26:21 +0100

    Added Core.Common.TestUtil
    - Project contains common test elements or helpers which can be used for unit testing.

---
+++ BalancedFieldLength/Simulator/Core/test/Core.Common.TestUtil/RandomExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Core.Common.Data;
+
+namespace Core.Common.TestUtil
+{
+    /// <summary>
+    /// Extension methods for <see cref="Random"/>.
+    /// </summary>
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Creates an <see cref="Angle"/> with random values.
+        /// </summary>
+        /// <param name="random">A pseudo-random generator.</param>
+        /// <returns>An <see cref="Angle"/> with random values.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="random"/>
+        /// is <c>null</c>.</exception>
+        public static Angle NextAngle(this Random random)
+        {
+            if (random == null)
+            {
+                throw new ArgumentNullException(nameof(random));
+            }
+
+            return Angle.FromDegrees(random.NextDouble());
+        }
+    }
+}
