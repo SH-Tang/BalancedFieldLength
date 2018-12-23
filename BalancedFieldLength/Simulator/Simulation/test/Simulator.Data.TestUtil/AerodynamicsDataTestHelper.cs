4b13a8b5b75fc4c55fb54a2b36ae559fb50348b1
Dennis Tang <dennis.tang@deltares.nl>
2018-12-23 14:51:44 +0100

    Introduced testhelper:
    Testhelper can be used to generate valid angles of attacks based on the aerodynamics data object.

---
+++ BalancedFieldLength/Simulator/Simulation/test/Simulator.Data.TestUtil/AerodynamicsDataTestHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Core.Common.Data;
+
+namespace Simulator.Data.TestUtil
+{
+    /// <summary>
+    /// Test helper which can be used for testing <see cref="AerodynamicsData"/>.
+    /// </summary>
+    public static class AerodynamicsDataTestHelper
+    {
+        /// <summary>
+        /// Gets a valid angle of attack (alpha) based on <paramref name="data"/>.
+        /// </summary>
+        /// <param name="data">The <see cref="AerodynamicsData"/> to generate a
+        /// valid angle of attack for.</param>
+        /// <returns>A valid angle of attack.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="data"/>
+        /// is <c>null</c>.</exception>
+        /// <remarks>As the angle of attack is defined as:
+        /// <code>
+        /// Flight Path Angle + Angle Of Attack = Pitch Angle
+        /// </code>
+        /// By setting the pitch angle equal to the angle of attack and
+        /// flight path angle to zero, the desired result can be obtained.
+        /// </remarks>
+        public static Angle GetValidAngleOfAttack(AerodynamicsData data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            return data.ZeroLiftAngleOfAttack + GetMidPoint(data.MaximumLiftCoefficient, data.LiftCoefficientGradient);
+        }
+
+        private static Angle GetMidPoint(double maximumLiftCoefficient, double liftGradient)
+        {
+            var midPointAngle = maximumLiftCoefficient / (2 * liftGradient);
+            return Angle.FromRadians(midPointAngle);
+        }
+    }
+}
