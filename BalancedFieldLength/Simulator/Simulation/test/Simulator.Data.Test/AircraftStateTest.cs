dc0c501dea7f5bb965994989c011f3248b622598
Dennis Tang <dennis.tang@deltares.nl>
2018-12-26 18:30:44 +0100

    Added test to verify the default values of the aircraft state.

---
+++ BalancedFieldLength/Simulator/Simulation/test/Simulator.Data.Test/AircraftStateTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Simulator.Data.Test
+{
+    [TestFixture]
+    public class AircraftStateTest
+    {
+        [Test]
+        public void DefaultConstructor_ExpectedValues()
+        {
+            // Call
+            var state = new AircraftState();
+
+            // Assert
+            Assert.Zero(state.FlightPathAngle.Radians);
+            Assert.Zero(state.PitchAngle.Radians);
+            Assert.Zero(state.TrueAirspeed);
+            Assert.Zero(state.Height);
+            Assert.Zero(state.Distance);
+        }
+    }
+}
