fe42aad6d3e3020d292bc082aedc79ae91dad140
Dennis Tang <dennis.sht@gmail.com>
2019-01-08 19:46:40 +0100

    Extracted interface out of the distance calculator for testing purposes

---
+++ BalancedFieldLength/Simulator/Simulation/src/Simulator.Calculator/IDistanceCalculator.cs
@@ -0,0 +1,19 @@
+﻿using Simulator.Data;
+using Simulator.Data.Exceptions;
+
+namespace Simulator.Calculator
+{
+    /// <summary>
+    /// Interface describing the calculator for calculating the traversed distance.
+    /// </summary>
+    public interface IDistanceCalculator
+    {
+        /// <summary>
+        /// Calculates the distance which is needed before the aircraft reaches either the screen height
+        /// or comes to a standstill.
+        /// </summary>
+        /// <returns>The <see cref="DistanceCalculatorOutput"/> with the calculated result.</returns>
+        /// <exception cref="CalculatorException">Thrown when the calculator cannot calculate the covered distance.</exception>
+        DistanceCalculatorOutput Calculate();
+    }
+}
