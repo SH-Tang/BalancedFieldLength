5ba43dd14fe2e4e9ce8d358d1b66456f572235e6
Dennis Tang <dennis.sht@gmail.com>
2019-03-08 22:10:58 +0100

    Extracted interface of the aggregated distance calculator to enable stubbing the calculation

--- BalancedFieldLength/Simulator/Simulation/src/Simulator.Calculator/AggregatedDistanceCalculator/AggregatedDistanceCalculator.cs
+++ BalancedFieldLength/Simulator/Simulation/src/Simulator.Calculator/AggregatedDistanceCalculator/AggregatedDistanceCalculator.cs
@@ -2,14 +2,13 @@
 using Simulator.Calculator.Factories;
 using Simulator.Calculator.Integrators;
 using Simulator.Data;
-using Simulator.Data.Exceptions;
 
 namespace Simulator.Calculator.AggregatedDistanceCalculator
 {
     /// <summary>
     /// Calculator to calculate the aggregated output for a given failure velocity.
     /// </summary>
-    public class AggregatedDistanceCalculator
+    public class AggregatedDistanceCalculator : IAggregatedDistanceCalculator
     {
         private readonly IDistanceCalculatorFactory distanceCalculatorFactory;
 
@@ -28,20 +27,6 @@ namespace Simulator.Calculator.AggregatedDistanceCalculator
             this.distanceCalculatorFactory = distanceCalculatorFactory;
         }
 
-        /// <summary>
-        /// Calculates the required distance for a continued and a rejected take off for a given failure speed.
-        /// </summary>
-        /// <param name="aircraftData">The <see cref="AircraftData"/> to create a <see cref="DistanceCalculator"/> for.</param>
-        /// <param name="integrator">The <see cref="IIntegrator"/> to solve the dynamic system.</param>
-        /// <param name="nrOfFailedEngines">The number of failed engines.</param>
-        /// <param name="density">The air density. [kg/m^3]</param>
-        /// <param name="gravitationalAcceleration">The gravitational acceleration. [m/s^2]</param>
-        /// <param name="calculationSettings">The <see cref="CalculationSettings"/> to configure the calculator.</param>
-        /// <returns>A <see cref="AggregatedDistanceOutput"/>.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="aircraftData"/>, <paramref name="integrator"/>
-        /// or <see cref="CalculationSettings"/> is <c>null</c>.</exception>
-        /// <exception cref="CalculatorException">Thrown when the <see cref="AggregatedDistanceOutput"/>
-        /// could not be calculated.</exception>
         public AggregatedDistanceOutput Calculate(AircraftData aircraftData,
                                                   IIntegrator integrator,
                                                   int nrOfFailedEngines,
@@ -66,7 +51,7 @@ namespace Simulator.Calculator.AggregatedDistanceCalculator
 
             IDistanceCalculator abortedTakeOffCalculator = distanceCalculatorFactory.CreateAbortedTakeOffDistanceCalculator(aircraftData,
                                                                                                                             integrator,
-                                                                                                                            density, 
+                                                                                                                            density,
                                                                                                                             gravitationalAcceleration,
                                                                                                                             calculationSettings);
 
@@ -80,8 +65,8 @@ namespace Simulator.Calculator.AggregatedDistanceCalculator
             DistanceCalculatorOutput abortedTakeOffOutput = abortedTakeOffCalculator.Calculate();
             DistanceCalculatorOutput continuedTakeOffOutput = continuedTakeOffCalculator.Calculate();
 
-            return new AggregatedDistanceOutput(calculationSettings.FailureSpeed, 
-                                                abortedTakeOffOutput.Distance, 
+            return new AggregatedDistanceOutput(calculationSettings.FailureSpeed,
+                                                abortedTakeOffOutput.Distance,
                                                 continuedTakeOffOutput.Distance);
         }
     }
---
+++ BalancedFieldLength/Simulator/Simulation/src/Simulator.Calculator/AggregatedDistanceCalculator/IAggregatedDistanceCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Simulator.Calculator.Integrators;
+using Simulator.Data;
+using Simulator.Data.Exceptions;
+
+namespace Simulator.Calculator.AggregatedDistanceCalculator
+{
+    /// <summary>
+    /// Interface which describes the calculation of the rejected and continued take off
+    /// distances for a given failure speed.
+    /// </summary>
+    public interface IAggregatedDistanceCalculator
+    {
+        /// <summary>
+        /// Calculates the required distance for a continued and a rejected take off for a given failure speed.
+        /// </summary>
+        /// <param name="aircraftData">The <see cref="AircraftData"/> to create a <see cref="DistanceCalculator"/> for.</param>
+        /// <param name="integrator">The <see cref="IIntegrator"/> to solve the dynamic system.</param>
+        /// <param name="nrOfFailedEngines">The number of failed engines.</param>
+        /// <param name="density">The air density. [kg/m^3]</param>
+        /// <param name="gravitationalAcceleration">The gravitational acceleration. [m/s^2]</param>
+        /// <param name="calculationSettings">The <see cref="CalculationSettings"/> to configure the calculator.</param>
+        /// <returns>A <see cref="AggregatedDistanceOutput"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="aircraftData"/>, <paramref name="integrator"/>
+        /// or <see cref="CalculationSettings"/> is <c>null</c>.</exception>
+        /// <exception cref="CalculatorException">Thrown when the <see cref="AggregatedDistanceOutput"/>
+        /// could not be calculated.</exception>
+        AggregatedDistanceOutput Calculate(AircraftData aircraftData,
+                                           IIntegrator integrator,
+                                           int nrOfFailedEngines,
+                                           double density,
+                                           double gravitationalAcceleration,
+                                           CalculationSettings calculationSettings);
+    }
+}
