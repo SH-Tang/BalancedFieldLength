166d24093d7ed1ebe5665a7ee80c6832b2c6e461
Dennis Tang <dennis.tang@deltares.nl>
2019-03-28 19:09:34 +0100

    Added integration object:
    - Contains a fully configured calculator to calculate the aggregated distance output with the components in integration.

---
+++ BalancedFieldLength/Simulator/Simulation/src/Simulator.Kernel/KernelValidationResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simulator.Kernel
+{
+    /// <summary>
+    /// Class which holds the validation result after validating data that is used by the kernel.
+    /// </summary>
+    public class KernelValidationResult
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="KernelValidationResult"/>.
+        /// </summary>
+        /// <param name="isValid">Indicator whether the data was valid.</param>
+        /// <param name="validationErrors">A collection containing the errors that occurred during the validation.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="validationErrors"/>
+        /// is <c>null</c>.</exception>
+        internal KernelValidationResult(bool isValid, IEnumerable<KernelValidationError> validationErrors)
+        {
+            ValidationErrors = validationErrors;
+            IsValid = isValid;
+        }
+
+        /// <summary>
+        /// Indicator whether the input is valid.
+        /// </summary>
+        public bool IsValid { get; }
+
+        /// <summary>
+        /// Gets the validation messages.
+        /// </summary>
+        public IEnumerable<KernelValidationError> ValidationErrors { get; }
+    }
+}
---
+++ BalancedFieldLength/Simulator/Simulation/test/Simulator.Kernel.Test/AggregatedDistanceCalculatorKernelTest.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using NUnit.Framework;
+using Simulator.Data;
+using Simulator.Data.TestUtil;
+
+namespace Simulator.Kernel.Test
+{
+    [TestFixture]
+    public class AggregatedDistanceCalculatorKernelTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var kernel = new AggregatedDistanceCalculatorKernel();
+
+            // Assert
+            Assert.IsInstanceOf<IAggregatedDistanceCalculatorKernel>(kernel);
+        }
+
+        [Test]
+        public void Validate_AircraftDataNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+            double density = random.NextDouble();
+            double gravitationalAcceleration = random.NextDouble();
+            int nrOfFailedEngines = random.Next();
+
+            var kernel = new AggregatedDistanceCalculatorKernel();
+
+            // Call
+            TestDelegate call = () => kernel.Validate(null, density, gravitationalAcceleration, nrOfFailedEngines);
+
+            // Assert
+            var exception = Assert.Throws<ArgumentNullException>(call);
+            Assert.AreEqual("aircraftData", exception.ParamName);
+        }
+
+        [Test]
+        public void Validate_WithValidData_ReturnsExpectedValidationResult()
+        {
+            // Setup
+            var random = new Random(21);
+            AircraftData aircraftData = AircraftDataTestFactory.CreateRandomAircraftData();
+            double density = random.NextDouble();
+            double gravitationalAcceleration = random.NextDouble();
+
+            var kernel = new AggregatedDistanceCalculatorKernel();
+
+            // Call
+            KernelValidationResult result = kernel.Validate(aircraftData,
+                                                            density,
+                                                            gravitationalAcceleration,
+                                                            aircraftData.NrOfEngines - 1);
+
+            // Assert
+            Assert.IsTrue(result.IsValid);
+            CollectionAssert.IsEmpty(result.ValidationErrors);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void Validate_DensityInvalid_ReturnsExpectedValidationResult(double density)
+        {
+            // Setup
+            var random = new Random(21);
+            AircraftData aircraftData = AircraftDataTestFactory.CreateRandomAircraftData();
+            double gravitationalAcceleration = random.NextDouble();
+
+            var kernel = new AggregatedDistanceCalculatorKernel();
+
+            // Call
+            KernelValidationResult result = kernel.Validate(aircraftData,
+                                                            density,
+                                                            gravitationalAcceleration,
+                                                            aircraftData.NrOfEngines - 1);
+
+            // Assert
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.AreEqual(new[]
+                                      {
+                                          KernelValidationError.InvalidDensity
+                                      }, result.ValidationErrors);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void Validate_GravityInvalid_ReturnsExpectedValidationResult(double gravitationalAcceleration)
+        {
+            // Setup
+            var random = new Random(21);
+            AircraftData aircraftData = AircraftDataTestFactory.CreateRandomAircraftData();
+            double density = random.NextDouble();
+
+            var kernel = new AggregatedDistanceCalculatorKernel();
+
+            // Call
+            KernelValidationResult result = kernel.Validate(aircraftData,
+                                                            density,
+                                                            gravitationalAcceleration,
+                                                            aircraftData.NrOfEngines - 1);
+
+            // Assert
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.AreEqual(new[]
+                                      {
+                                          KernelValidationError.InvalidGravitationalAcceleration
+                                      }, result.ValidationErrors);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        public void Validate_NrOfFailedEnginesInvalid_ReturnsExpectedValidationResult(int offset)
+        {
+            // Setup
+            var random = new Random(21);
+            AircraftData aircraftData = AircraftDataTestFactory.CreateRandomAircraftData();
+            double density = random.NextDouble();
+            double gravitationalAcceleration = random.NextDouble();
+
+            var kernel = new AggregatedDistanceCalculatorKernel();
+
+            // Call
+            KernelValidationResult result = kernel.Validate(aircraftData,
+                                                            density,
+                                                            gravitationalAcceleration,
+                                                            aircraftData.NrOfEngines + offset);
+
+            // Assert
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.AreEqual(new[]
+                                      {
+                                          KernelValidationError.InvalidNrOfFailedEngines
+                                      }, result.ValidationErrors);
+        }
+
+        [Test]
+        public void Validate_AllDataInvalid_ReturnsExpectedValidationResult()
+        {
+            // Setup
+            AircraftData aircraftData = AircraftDataTestFactory.CreateRandomAircraftData();
+
+            var kernel = new AggregatedDistanceCalculatorKernel();
+
+            // Call
+            KernelValidationResult result = kernel.Validate(aircraftData, 0, 0,
+                                                            aircraftData.NrOfEngines + 1);
+
+            // Assert
+            Assert.IsFalse(result.IsValid);
+            CollectionAssert.AreEquivalent(new[]
+                                           {
+                                               KernelValidationError.InvalidDensity,
+                                               KernelValidationError.InvalidGravitationalAcceleration,
+                                               KernelValidationError.InvalidNrOfFailedEngines
+                                           }, result.ValidationErrors);
+        }
+    }
+}
