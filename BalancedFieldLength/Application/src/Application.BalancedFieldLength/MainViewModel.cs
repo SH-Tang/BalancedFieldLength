ac0c2d672f5616e3fd58bb48596d9553b108a039
Dennis Tang <dennis.sht@gmail.com>
2019-08-16 21:16:30 +0200

    Added a MainViewModel:
    - Introduced tabs for initial mockup of the GUI

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Application.BalancedFieldLength.Controls;
+
+namespace Application.BalancedFieldLength
+{
+    /// <summary>
+    /// The view model of the application.
+    /// </summary>
+    public class MainViewModel
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="MainViewModel"/>.
+        /// </summary>
+        public MainViewModel()
+        {
+            var tabControlViewModel = new TabControlViewModel();
+            var generalSettingsTab = new GeneralSimulationSettingsTabViewModel();
+            tabControlViewModel.Tabs.Add(generalSettingsTab);
+            tabControlViewModel.SelectedTabItem = generalSettingsTab;
+
+            TabControlViewModel = tabControlViewModel;
+        }
+
+        public TabControlViewModel TabControlViewModel { get; }
+    }
+}
