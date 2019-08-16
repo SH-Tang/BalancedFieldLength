26582cf9a9a51794a353610b4ac437862de3ed18
Dennis Tang <dennis.sht@gmail.com>
2019-08-16 17:31:29 +0200

    Added Application.BalancedFieldLength project to host the GUI interface

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyTitle("Application.BalancedFieldLength")]
+[assembly: AssemblyProduct("Application.BalancedFieldLength")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page,
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page,
+    // app, or any theme specific resource dictionaries)
+)]
