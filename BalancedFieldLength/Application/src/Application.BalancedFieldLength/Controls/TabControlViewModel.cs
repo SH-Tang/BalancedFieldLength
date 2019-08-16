63e32ec9405671b9f6495a3e2c783d4420ea028c
Dennis Tang <dennis.sht@gmail.com>
2019-08-16 18:42:11 +0200

    Added TabControl:
    - Added generic view model for an user control to hold tab items

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength/Controls/TabControlViewModel.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace Application.BalancedFieldLength.Controls
+{
+    /// <summary>
+    /// The view model for a control that hosts tabs.
+    /// </summary>
+    public class TabControlViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="TabControlViewModel"/>.
+        /// </summary>
+        public TabControlViewModel()
+        {
+            Tabs = new ObservableCollection<ITabViewModel>();
+            Tabs.CollectionChanged += OnTabItemsChanged;
+        }
+
+        /// <summary>
+        /// Gets the collection of tabs that are present within this control.
+        /// </summary>
+        public ObservableCollection<ITabViewModel> Tabs { get; }
+
+        private void OnTabItemsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null && e.NewItems.Count != 0)
+            {
+                foreach (ITabViewModel tab in e.NewItems)
+                {
+                    tab.PropertyChanged += TabPropertyChanged;
+                }
+            }
+
+            if (e.OldItems != null && e.OldItems.Count != 0)
+            {
+                foreach (ITabViewModel tab in e.OldItems)
+                {
+                    tab.PropertyChanged -= TabPropertyChanged;
+                }
+            }
+        }
+
+        private void TabPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(sender, e);
+        }
+    }
+}
---
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.Test/Controls/TabControlViewModelTest.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Application.BalancedFieldLength.Controls;
+using NUnit.Framework;
+
+namespace Application.BalancedFieldLength.Test.Controls
+{
+    [TestFixture]
+    public class TabControlViewModelTest
+    {
+        [Test]
+        public static void Constructor_ExpectedValues()
+        {
+            // Call
+            var viewModel = new TabControlViewModel();
+
+            // Assert
+            Assert.That(viewModel, Is.InstanceOf<INotifyPropertyChanged>());
+            Assert.That(viewModel.Tabs, Is.Empty);
+        }
+
+        [Test]
+        public void GivenTabControlViewModelWithTabs_WhenTabRaisesPropertyChangedEvent_ThenTabControlViewRaisesPropertyChangedEvent()
+        {
+            // Given
+            var viewModel = new TabControlViewModel();
+
+            PropertyChangedEventArgs eventArgs = null;
+            bool propertyChangedTriggered = false;
+            viewModel.PropertyChanged += (o, e) =>
+                                         {
+                                             eventArgs = e;
+                                             propertyChangedTriggered = true;
+                                         };
+
+            var tabViewModel = new TestTabViewModel();
+            viewModel.Tabs.Add(tabViewModel);
+
+            var random = new Random(21);
+
+            // When 
+            tabViewModel.TabProperty = random.Next();
+
+            // Then
+            Assert.That(propertyChangedTriggered, Is.True);
+            Assert.That(eventArgs, Is.Not.Null);
+            Assert.That(eventArgs.PropertyName, Is.EqualTo(nameof(TestTabViewModel.TabProperty)));
+        }
+
+        [Test]
+        public void GivenTabControlViewModelWithTabs_WhenRemovingTabAndRemovedTabRaisesPropertyChangedEvent_ThenTabControlDoesNotRaiseEvent()
+        {
+            // Given
+            var viewModel = new TabControlViewModel();
+
+            PropertyChangedEventArgs eventArgs = null;
+            bool propertyChangedTriggered = false;
+            viewModel.PropertyChanged += (o, e) =>
+                                         {
+                                             eventArgs = e;
+                                             propertyChangedTriggered = true;
+                                         };
+
+            var tabViewModel = new TestTabViewModel();
+            viewModel.Tabs.Add(tabViewModel);
+
+            var random = new Random(21);
+
+            // When 
+            bool removeSuccessful = viewModel.Tabs.Remove(tabViewModel);
+            Assert.That(removeSuccessful, Is.True);
+
+            tabViewModel.TabProperty = random.Next();
+
+            // Then
+            Assert.That(propertyChangedTriggered, Is.False);
+            Assert.That(eventArgs, Is.Null);
+        }
+
+
+        private class TestTabViewModel : ITabViewModel
+        {
+            private int tabProperty;
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public int TabProperty
+            {
+                get
+                {
+                    return tabProperty;
+                }
+                set
+                {
+                    tabProperty = value;
+                    OnPropertyChanged(nameof(TabProperty));
+                }
+            }
+
+            public string TabName
+            {
+                get
+                {
+                    throw new NotSupportedException();
+                }
+            }
+
+            private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
