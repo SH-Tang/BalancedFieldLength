28aef6abc89a5a2d0a7bbe207396131f800a6fc7
Dennis Tang <dennis.sht@gmail.com>
2019-08-17 09:58:08 +0200

    Converted look and feel to a Metro layout

--- BalancedFieldLength/Application/src/Application.BalancedFieldLength/MainWindow.xaml.cs
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength/MainWindow.xaml.cs
@@ -15,26 +15,14 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace Application.BalancedFieldLength
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
