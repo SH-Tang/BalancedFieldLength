c52356841a55db956f7f1e2ea36fd6ddf648e2af
Dennis Tang <dennis.sht@gmail.com>
2019-08-30 18:36:16 +0200

    Added message window.

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength/Controls/MessageContext.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+namespace Application.BalancedFieldLength.Controls {
+    /// <summary>
+    /// A class containing the context of which a message was generated.
+    /// </summary>
+    public class MessageContext
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="MessageContext"/>.
+        /// </summary>
+        /// <param name="messageType">The <see cref="MessageType"/>.</param>
+        /// <param name="message">The message.</param>
+        public MessageContext(MessageType messageType, string message)
+        {
+            MessageType = messageType;
+            Message = message;
+        }
+
+        /// <summary>
+        /// Gets the message type.
+        /// </summary>
+        public MessageType MessageType { get; }
+
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        public string Message { get; }
+    }
+}
---
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.Test/Controls/MessageViewModelTest.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System.ComponentModel;
+using Application.BalancedFieldLength.Controls;
+using Application.BalancedFieldLength.WPFCommon;
+using NUnit.Framework;
+
+namespace Application.BalancedFieldLength.Test.Controls
+{
+    [TestFixture]
+    public class MessageViewModelTest
+    {
+        [Test]
+        public static void Constructor_ExpectedValues()
+        {
+            // Call
+            var viewModel = new MessageWindowViewModel();
+
+            // Assert
+            Assert.That(viewModel, Is.InstanceOf<ViewModelBase>());
+            Assert.That(viewModel.Messages, Is.Not.Null.And.Empty);
+        }
+
+        [Test]
+        public void AddMessage_MessageNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var viewModel = new MessageWindowViewModel();
+
+            // Call 
+            TestDelegate call = () => viewModel.AddMessage(null);
+
+            // Assert
+            Assert.That(call, Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void AddMessage_MessageValid_AddsMessageToCollectionAndNotifyPropertyChanged()
+        {
+            // Setup
+            var viewModel = new MessageWindowViewModel();
+
+            bool propertyChangedTriggered = false;
+            PropertyChangedEventArgs eventArgs = null;
+            viewModel.PropertyChanged += (o, e) =>
+            {
+                propertyChangedTriggered = true;
+                eventArgs = e;
+            };
+
+            var message = new MessageContext(MessageType.Warning, string.Empty);
+
+            // Call 
+            viewModel.AddMessage(message);
+
+            // Assert
+            Assert.That(propertyChangedTriggered, Is.True);
+            Assert.That(eventArgs.PropertyName, Is.EqualTo(nameof(viewModel.Messages)));
+        }
+    }
+}
