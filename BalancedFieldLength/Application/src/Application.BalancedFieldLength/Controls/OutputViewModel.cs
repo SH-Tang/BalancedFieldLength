adc92320c87910aa6eb847c4a09a0d8387cf4bae
Dennis Tang <dennis.sht@gmail.com>
2019-09-01 08:45:34 +0200

    Added view model to display the output.

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength/Controls/OutputViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Application.BalancedFieldLength.Controls
+{
+    /// <summary>
+    /// View model for displaying the output.
+    /// </summary>
+    public class OutputViewModel
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="OutputViewModel"/>.
+        /// </summary>
+        public OutputViewModel()
+        {
+            BalancedFieldLengthDistance = double.NaN;
+            BalancedFieldLengthVelocity = double.NaN;
+        }
+
+        /// <summary>
+        /// Gets the balanced field length distance. [m]
+        /// </summary>
+        public double BalancedFieldLengthDistance { get; }
+
+        /// <summary>
+        /// Gets the velocity at which the balanced field length
+        /// distance occurs. [m/s]
+        /// </summary>
+        public double BalancedFieldLengthVelocity { get; }
+    }
+}
---
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.Test/Controls/OutputViewModelTest.cs
@@ -0,0 +1,20 @@
+﻿using Application.BalancedFieldLength.Controls;
+using NUnit.Framework;
+
+namespace Application.BalancedFieldLength.Test.Controls
+{
+    [TestFixture]
+    public class OutputViewModelTest
+    {
+        [Test]
+        public static void Constructor_ExpectedValues()
+        {
+            // Call
+            var viewModel = new OutputViewModel();
+
+            // Assert
+            Assert.That(viewModel.BalancedFieldLengthDistance, Is.NaN);
+            Assert.That(viewModel.BalancedFieldLengthVelocity, Is.NaN);
+        }
+    }
+}
