b8912752c66600125df5f30d51348f90aa265810
Dennis Tang <dennis.sht@gmail.com>
2019-12-20 18:02:15 +0100

    Introduced object to hold engine data

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength.Data/EngineData.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+namespace Application.BalancedFieldLength.Data {
+    /// <summary>
+    /// Class to hold the engine data.
+    /// </summary>
+    public class EngineData
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="EngineData"/>.
+        /// </summary>
+        public EngineData()
+        {
+            ThrustPerEngine = double.NaN;
+        }
+
+        /// <summary>
+        /// Gets or sets the total number of engines.
+        /// </summary>
+        public int NrOfEngines { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total number of failed engines.
+        /// </summary>
+        public int NrOfFailedEngines { get; set; }
+
+        /// <summary>
+        /// Gets or sets the thrust per engine. [kN]
+        /// </summary>
+        public double ThrustPerEngine { get; set; }
+    }
+}
---
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.Data.Test/EngineDataTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Application.BalancedFieldLength.Data.Test
+{
+    [TestFixture]
+    public class EngineDataTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var engineData = new EngineData();
+
+            // Assert
+            Assert.That(engineData.NrOfEngines, Is.Zero);
+            Assert.That(engineData.NrOfFailedEngines, Is.Zero);
+            Assert.That(engineData.ThrustPerEngine, Is.NaN);
+        }
+    }
+}
