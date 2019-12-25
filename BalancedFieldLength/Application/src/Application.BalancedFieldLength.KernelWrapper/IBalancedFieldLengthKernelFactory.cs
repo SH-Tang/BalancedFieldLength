3dc2354a4f3a898e41c9972fcfc793341af40823
Dennis Tang <dennis.sht@gmail.com>
2019-12-25 10:11:35 +0100

    Added factory to create calculation kernels. Implemented according to the shunt pattern to allow substitution of the interface

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength.KernelWrapper/IBalancedFieldLengthKernelFactory.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using Simulator.Kernel;
+
+namespace Application.BalancedFieldLength.KernelWrapper
+{
+    /// <summary>
+    /// Interface for describing the creation of kernels to perform calculations with.
+    /// </summary>
+    public interface IBalancedFieldLengthKernelFactory
+    {
+        /// <summary>
+        /// Creates a kernel to perform the balanced field length calculation with.
+        /// </summary>
+        /// <returns>A new <see cref="IAggregatedDistanceCalculatorKernel"/>.</returns>
+        IAggregatedDistanceCalculatorKernel CreateDistanceCalculatorKernel();
+    }
+}
