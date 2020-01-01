27e85bc4416b782de035a2858cd576041c50fa6e
Dennis Tang <dennis.sht@gmail.com>
2020-01-01 10:14:14 +0100

    Added factory to create calculation settings.

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength.KernelWrapper/Factories/CalculationSettingsFactory.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Application.BalancedFieldLength.Data;
+using Application.BalancedFieldLength.KernelWrapper.Exceptions;
+using Simulator.Data;
+
+namespace Application.BalancedFieldLength.KernelWrapper.Factories
+{
+    /// <summary>
+    /// Factory to create <see cref="CalculationSettings"/>.
+    /// </summary>
+    public static class CalculationSettingsFactory
+    {
+        /// <summary>
+        /// Creates a <see cref="CalculationSettings"/> based on <see cref="GeneralSimulationSettingsData"/>.
+        /// </summary>
+        /// <param name="settings">The <see cref="GeneralSimulationSettingsData"/> to create
+        /// a <see cref="CalculationSettings"/> for.</param>
+        /// <param name="failureSpeed">The failure speed to create the settings for. [m/s]</param>
+        /// <returns>A <see cref="CalculationSettings"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="settings"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="CreateKernelDataException">Thrown when <see cref="CalculationSettings"/>
+        /// could not be successfully created.</exception>
+        public static CalculationSettings Create(GeneralSimulationSettingsData settings, int failureSpeed)
+        {
+            if (settings == null)
+            {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            try
+            {
+                return new CalculationSettings(failureSpeed, settings.MaximumNrOfIterations, settings.TimeStep);
+            }
+            catch (ArgumentException e)
+            {
+                throw new CreateKernelDataException(e.Message, e);
+            }
+        }
+    }
+}
---
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.KernelWrapper.Test/Factories/CalculationSettingsFactoryTest.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Application.BalancedFieldLength.Data;
+using Application.BalancedFieldLength.KernelWrapper.Exceptions;
+using Application.BalancedFieldLength.KernelWrapper.Factories;
+using NUnit.Framework;
+using Simulator.Data;
+
+namespace Application.BalancedFieldLength.KernelWrapper.Test.Factories
+{
+    [TestFixture]
+    public class CalculationSettingsFactoryTest
+    {
+        [Test]
+        public void Create_SettingsNull_ThrowsArgumentNullException()
+        {
+            // Setup
+            var random = new Random(21);
+
+            // Call
+            TestDelegate call = () => CalculationSettingsFactory.Create(null, random.Next());
+
+            // Assert
+            Assert.That(call, Throws.ArgumentNullException
+                                    .With.Property(nameof(ArgumentNullException.ParamName))
+                                    .EqualTo("settings"));
+        }
+
+        [Test]
+        public void Create_WithValidGeneralSimulationSettingsData_ReturnsExpectedCalculationSettings()
+        {
+            // Setup
+            var random = new Random(21);
+            int failureSpeed = random.Next();
+
+            var settings = new GeneralSimulationSettingsData
+            {
+                TimeStep = random.NextDouble(),
+                MaximumNrOfIterations = random.Next()
+            };
+
+            // Call 
+            CalculationSettings calculationSettings = CalculationSettingsFactory.Create(settings, failureSpeed);
+
+            // Assert
+            Assert.That(calculationSettings.TimeStep, Is.EqualTo(settings.TimeStep));
+            Assert.That(calculationSettings.MaximumNrOfTimeSteps, Is.EqualTo(settings.MaximumNrOfIterations));
+            Assert.That(calculationSettings.FailureSpeed, Is.EqualTo(failureSpeed));
+        }
+
+        [Test]
+        public void Create_WithGeneralSimulationSettingsDataResultingInArgumentException_ThrowsKernelDataCreateException()
+        {
+            // Setup
+            var random = new Random(21);
+
+            var settings = new GeneralSimulationSettingsData
+            {
+                TimeStep = double.NaN,
+                MaximumNrOfIterations = random.Next()
+            };
+
+            // Call 
+            TestDelegate call = () => CalculationSettingsFactory.Create(settings, random.Next());
+
+            // Assert
+            var exception = Assert.Throws<CreateKernelDataException>(call);
+            Exception innerException = exception.InnerException;
+            Assert.That(innerException, Is.TypeOf<ArgumentException>());
+            Assert.That(exception.Message, Is.EqualTo(innerException.Message));
+        }
+
+        [Test]
+        public void Create_WithGeneralSimulationSettingsDataResultingInArgumentOutOfRangeException_ThrowsKernelDataCreateException()
+        {
+            // Setup
+            var random = new Random(21);
+
+            var settings = new GeneralSimulationSettingsData
+            {
+                TimeStep = random.NextDouble(),
+                MaximumNrOfIterations = -1 * random.Next()
+            };
+
+            // Call 
+            TestDelegate call = () => CalculationSettingsFactory.Create(settings, random.Next());
+
+            // Assert
+            var exception = Assert.Throws<CreateKernelDataException>(call);
+            Exception innerException = exception.InnerException;
+            Assert.That(innerException, Is.TypeOf<ArgumentOutOfRangeException>());
+            Assert.That(exception.Message, Is.EqualTo(innerException.Message));
+        }
+    }
+}
