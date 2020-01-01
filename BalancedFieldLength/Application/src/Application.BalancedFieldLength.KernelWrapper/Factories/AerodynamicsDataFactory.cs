c1c2e7a33c1b5483eac5f6c2ad03906feb148bd4
Dennis Tang <dennis.sht@gmail.com>
2020-01-01 09:12:56 +0100

    Added initial commit of factory methods to create kernel input based on the application data

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength.KernelWrapper/Factories/AerodynamicsDataFactory.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Application.BalancedFieldLength.Data;
+using KernelAerodynamicData = Simulator.Data.AerodynamicsData;
+
+namespace Application.BalancedFieldLength.KernelWrapper.Factories
+{
+    /// <summary>
+    /// Factory for creating <see cref="Simulator.Data.AerodynamicsData"/>.
+    /// </summary>
+    public static class AerodynamicsDataFactory
+    {
+        /// <summary>
+        /// Creates an <see cref="Simulator.Data.AerodynamicsData"/> based on <see cref="AircraftData"/>.
+        /// </summary>
+        /// <param name="aircraftData">The <see cref="AircraftData"/> to create an
+        /// <see cref="Simulator.Data.AerodynamicsData"/> for.</param>
+        /// <returns>An <see cref="Simulator.Data.AerodynamicsData"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="aircraftData"/>
+        /// is <c>null</c>.</exception>
+        public static KernelAerodynamicData Create(AircraftData aircraftData)
+        {
+            if (aircraftData == null)
+            {
+                throw new ArgumentNullException(nameof(aircraftData));
+            }
+
+            return new KernelAerodynamicData(aircraftData.AspectRatio, aircraftData.WingSurfaceArea,
+                                             aircraftData.ZeroLiftAngleOfAttack, aircraftData.LiftCoefficientGradient,
+                                             aircraftData.MaximumLiftCoefficient, aircraftData.RestDragCoefficient,
+                                             aircraftData.RestDragCoefficientWithEngineFailure, aircraftData.OswaldFactor);
+        }
+    }
+}
---
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.KernelWrapper.Test/Factories/AeroDynamicsDataFactoryTest.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Application.BalancedFieldLength.Data;
+using Application.BalancedFieldLength.KernelWrapper.Factories;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using KernelAerodynamicsData = Simulator.Data.AerodynamicsData;
+
+namespace Application.BalancedFieldLength.KernelWrapper.Test.Factories
+{
+    [TestFixture]
+    public class AerodynamicsDataFactoryTest
+    {
+        [Test]
+        public void Create_AircraftDataNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => AerodynamicsDataFactory.Create(null);
+
+            // Assert
+            Assert.That(call, Throws.ArgumentNullException
+                                    .With.Property(nameof(ArgumentNullException.ParamName))
+                                    .EqualTo("aircraftData"));
+        }
+
+        [Test]
+        public void Create_WithCalculation_ReturnsExpectedAircraftData()
+        {
+            // Setup
+            var random = new Random(21);
+            var aircraftData = new AircraftData
+            {
+                WingSurfaceArea = random.NextDouble(),
+                AspectRatio = random.NextDouble(),
+                OswaldFactor = random.NextDouble(),
+                MaximumLiftCoefficient = random.NextDouble(),
+                LiftCoefficientGradient = random.NextDouble(),
+                ZeroLiftAngleOfAttack = random.NextAngle(),
+                RestDragCoefficient = random.NextDouble(),
+                RestDragCoefficientWithEngineFailure = random.NextDouble()
+            };
+
+            // Call 
+            KernelAerodynamicsData data = AerodynamicsDataFactory.Create(aircraftData);
+
+            // Assert
+            Assert.That(data.AspectRatio, Is.EqualTo(aircraftData.AspectRatio));
+            Assert.That(data.WingArea, Is.EqualTo(aircraftData.WingSurfaceArea));
+
+            Assert.That(data.LiftCoefficientGradient, Is.EqualTo(aircraftData.LiftCoefficientGradient));
+            Assert.That(data.MaximumLiftCoefficient, Is.EqualTo(aircraftData.MaximumLiftCoefficient));
+            Assert.That(data.ZeroLiftAngleOfAttack, Is.EqualTo(aircraftData.ZeroLiftAngleOfAttack));
+            Assert.That(data.OswaldFactor, Is.EqualTo(aircraftData.OswaldFactor));
+
+            Assert.That(data.RestDragCoefficientWithEngineFailure, Is.EqualTo(aircraftData.RestDragCoefficientWithEngineFailure));
+            Assert.That(data.RestDragCoefficientWithoutEngineFailure, Is.EqualTo(aircraftData.RestDragCoefficient));
+        }
+    }
+}
