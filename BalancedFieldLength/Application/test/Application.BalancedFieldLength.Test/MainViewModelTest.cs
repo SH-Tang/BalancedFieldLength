62552e3c565731d2c154c5e578e286e11ce70af5
Dennis Tang <karabasf@gmail.com>
2020-06-27 09:16:00 +0200

    Added tests to verify the calculate / simulate command

--- BalancedFieldLength/Application/test/Application.BalancedFieldLength.Test/MainViewModelTest.cs
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.Test/MainViewModelTest.cs
@@ -15,9 +15,16 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
+using Application.BalancedFieldLength.Data;
+using Application.BalancedFieldLength.KernelWrapper;
+using Application.BalancedFieldLength.KernelWrapper.TestUtils;
+using Application.BalancedFieldLength.Views.OutputView;
 using Application.BalancedFieldLength.Views.TabViews;
 using NUnit.Framework;
+using WPF.Components.MessageView;
 using WPF.Components.TabControl;
 
 namespace Application.BalancedFieldLength.Test
@@ -35,6 +42,7 @@ namespace Application.BalancedFieldLength.Test
             Assert.That(mainViewModel.OutputViewModel, Is.Null);
             Assert.That(mainViewModel.MessageWindowViewModel, Is.Not.Null);
             Assert.That(mainViewModel.TabControlViewModel, Is.Not.Null);
+            Assert.That(mainViewModel.CalculateCommand, Is.Not.Null);
 
             TabControlViewModel tabControlViewModel = mainViewModel.TabControlViewModel;
             ObservableCollection<ITabViewModel> tabs = tabControlViewModel.Tabs;
@@ -43,6 +51,96 @@ namespace Application.BalancedFieldLength.Test
             Assert.That(tabs[1], Is.TypeOf<EngineSettingsTabViewModel>());
             Assert.That(tabs[2], Is.TypeOf<AircraftDataTabViewModel>());
             Assert.That(tabControlViewModel.SelectedTabItem, Is.SameAs(tabs[0]));
+        }
+
+        [Test]
+        public void GivenViewModelAndCalculationSuccessful_WhenCalculating_ThenOutputGenerated()
+        {
+            // Given
+            var random = new Random(21);
+            var calculatedOutput = new BalancedFieldLengthOutput(random.NextDouble(), random.NextDouble());
+
+            var viewModel = new MainViewModel();
+
+            using (new BalancedFieldLengthCalculationModuleFactoryConfig())
+            {
+                var instance = (TestBalancedFieldLengthCalculationModuleFactory)
+                    BalancedFieldLengthCalculationModuleFactory.Instance;
+                
+                TestBalancedFieldLengthCalculationModule testModule = instance.TestModule;
+                testModule.Output = calculatedOutput;
+
+                // When
+                viewModel.CalculateCommand.Execute(null);
+
+                // Then
+                OutputViewModel outputViewModel = viewModel.OutputViewModel;
+                Assert.That(outputViewModel, Is.Not.Null);
+                Assert.That(outputViewModel.BalancedFieldLengthDistance, Is.EqualTo(calculatedOutput.Distance));
+                Assert.That(outputViewModel.BalancedFieldLengthVelocity, Is.EqualTo(calculatedOutput.Velocity));
+            }
+        }
+
+        [Test]
+        public void GivenViewModelAndCalculationThrowsCreateKernelDataException_WhenCalculating_ThenErrorMessagesLogged()
+        {
+            // Given
+            var viewModel = new MainViewModel();
+
+            using (new BalancedFieldLengthCalculationModuleFactoryConfig())
+            {
+                var instance = (TestBalancedFieldLengthCalculationModuleFactory)
+                    BalancedFieldLengthCalculationModuleFactory.Instance;
+
+                TestBalancedFieldLengthCalculationModule testModule = instance.TestModule;
+                testModule.ThrowCreateKernelDataException = true;
+
+                // Precondition
+                MessageWindowViewModel messageWindowViewModel = viewModel.MessageWindowViewModel;
+                Assert.That(messageWindowViewModel.Messages, Is.Empty);
+
+                // When
+                viewModel.CalculateCommand.Execute(null);
+
+                // Then
+                Assert.That(viewModel.OutputViewModel, Is.Null);
+
+                Assert.That(messageWindowViewModel.Messages, Has.Count.EqualTo(1));
+                MessageContext message = messageWindowViewModel.Messages.Single();
+                Assert.That(message.MessageType, Is.EqualTo(MessageType.Error));
+                Assert.That(message.Message, Is.EqualTo("Exception"));
+            }
+        }
+
+        [Test]
+        public void GivenViewModelAndCalculationThrowsKernelCalculationException_WhenCalculating_ThenErrorMessagesLogged()
+        {
+            // Given
+            var viewModel = new MainViewModel();
+
+            using (new BalancedFieldLengthCalculationModuleFactoryConfig())
+            {
+                var instance = (TestBalancedFieldLengthCalculationModuleFactory)
+                    BalancedFieldLengthCalculationModuleFactory.Instance;
+
+                TestBalancedFieldLengthCalculationModule testModule = instance.TestModule;
+                testModule.ThrowKernelCalculationException = true;
+
+                // Precondition
+                MessageWindowViewModel messageWindowViewModel = viewModel.MessageWindowViewModel;
+                Assert.That(messageWindowViewModel.Messages, Is.Empty);
+
+                // When
+                viewModel.CalculateCommand.Execute(null);
+
+                // Then
+                Assert.That(viewModel.OutputViewModel, Is.Null);
+
+                Assert.That(messageWindowViewModel.Messages, Has.Count.EqualTo(1));
+                MessageContext message = messageWindowViewModel.Messages.Single();
+                Assert.That(message.MessageType, Is.EqualTo(MessageType.Error));
+                Assert.That(message.Message, Is.EqualTo("Exception"));
+            }
         }
     }
 }
