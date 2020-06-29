fcdbea1599a17aeed50d277c2d0c481b2a8bfbd3
Dennis Tang <karabasf@gmail.com>
2020-06-29 21:41:39 +0200

    Introduced helpers:
    - Enter press to commit values
    - Enter press to move the focus around the control

---
+++ BalancedFieldLength/WPF/src/WPF.Components/TextBoxModificationBinding.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace WPF.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class TextBoxModificationBinding
+    {
+        /// <summary>
+        /// Creates a modification binding on the input argument.
+        /// </summary>
+        /// <param name="textBox">The <see cref="TextBox"/> to create the binding for.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="textBox" />
+        /// is <c>null</c>.</exception>
+        public static void Create(TextBox textBox)
+        {
+            if (textBox == null)
+            {
+                throw new ArgumentNullException(nameof(textBox));
+            }
+
+            BindingExpression textBoxBinding = textBox.GetBindingExpression(TextBox.TextProperty);
+            textBox.KeyDown += (sender, e) => OnKeyDown(e, textBoxBinding);
+            textBox.LostFocus += (sender, e) => OnLostFocus(textBoxBinding);
+        }
+
+        private static void OnKeyDown(KeyEventArgs e, BindingExpressionBase binding)
+        {
+            if (e.Key.Equals(Key.Return))
+            {
+                binding.UpdateSource();
+            }
+        }
+
+        private static void OnLostFocus(BindingExpressionBase binding)
+        {
+            binding.UpdateSource();
+        }
+    }
+}
