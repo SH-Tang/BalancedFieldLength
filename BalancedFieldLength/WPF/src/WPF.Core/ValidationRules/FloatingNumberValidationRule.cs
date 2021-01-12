1072af4543323a01ee14fa15829e0827585b0ce0
Dennis Tang <karabasf@gmail.com>
2021-01-12 17:08:04 +0100

    Added validationrule to verify floating numbers as user input

---
+++ BalancedFieldLength/WPF/src/WPF.Core/ValidationRules/FloatingNumberValidationRule.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace WPF.Core.ValidationRules
+{
+    /// <summary>
+    /// Validation rule for determining whether the user input is a floating number.
+    /// </summary>
+    public class FloatingNumberValidationRule : ValidationRule
+    {
+        private const NumberStyles numberStyle = NumberStyles.AllowLeadingSign
+                                                 | NumberStyles.AllowExponent
+                                                 | NumberStyles.AllowDecimalPoint
+                                                 | NumberStyles.AllowThousands;
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string stringValue = value as string;
+            if (stringValue == null)
+            {
+                return new ValidationResult(false, $"{nameof(value)} must be a string.");
+            }
+
+            if (string.IsNullOrWhiteSpace(stringValue))
+            {
+                return ValidationResult.ValidResult;
+            }
+
+            double parsedValue;
+            if (!double.TryParse(stringValue, numberStyle, CultureInfo.InvariantCulture, out parsedValue))
+            {
+                return new ValidationResult(false, $"{stringValue} could not be parsed as a floating number.");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
