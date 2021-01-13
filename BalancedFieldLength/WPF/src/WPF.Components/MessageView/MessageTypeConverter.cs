dc5cc3ed301f4142baf7ba854df6a188796d0b49
Dennis Tang <dennis.sht@gmail.com>
2021-01-13 17:57:08 +0100

    MessageType converter added to convert from enum to a Brush color.

---
+++ BalancedFieldLength/WPF/src/WPF.Components/MessageView/MessageTypeConverter.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WPF.Components.MessageView
+{
+    /// <summary>
+    /// Converter which converts the <see cref="MessageType"/>.
+    /// </summary>
+    public class MessageTypeConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        /// <exception cref="InvalidEnumArgumentException">Thrown when <paramref name="value"/>
+        /// is an invalid <see cref="MessageType"/>.</exception>
+        /// <exception cref="NotSupportedException">Thrown when:
+        /// <list type="bullet">
+        /// <item>The <paramref name="value"/> is not of type <see cref="MessageType"/>.</item>
+        /// <item>The <paramref name="value"/> is a valid <see cref="MessageType"/> but not supported.</item>
+        /// <item>the <paramref name="targetType"/> is not of type <see cref="Brush"/>
+        /// or of type <see cref="object"/>.</item>
+        /// </list></exception>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is MessageType))
+            {
+                throw new NotSupportedException($"Conversion from {value?.GetType().Name} is not supported.");
+            }
+
+            if (targetType != typeof(Brush) && targetType != typeof(object))
+            {
+                throw new NotSupportedException($"Conversion to {targetType.Name} is not supported.");
+            }
+
+            var valueToConvert = (MessageType) value;
+            if (!Enum.IsDefined(typeof(MessageType), valueToConvert))
+            {
+                throw new InvalidEnumArgumentException(nameof(value), (int) valueToConvert, typeof(MessageType));
+            }
+
+            switch (valueToConvert)
+            {
+                case MessageType.Info:
+                    return Brushes.CornflowerBlue;
+                case MessageType.Error:
+                    return Brushes.Crimson;
+                case MessageType.Warning:
+                    return Brushes.Gold;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="NotSupportedException">Thrown when function is called.</exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack operation is not supported.");
+        }
+    }
+}
---
+++ BalancedFieldLength/WPF/test/WPF.Components.Test/MessageView/MessageTypeConverterTest.cs
@@ -0,0 +1,153 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Media;
+using Core.Common.TestUtil;
+using NUnit.Framework;
+using WPF.Components.MessageView;
+
+namespace WPF.Components.Test.MessageView
+{
+    [TestFixture]
+    public class MessageTypeConverterTest
+    {
+        [Test]
+        public void Constructor_ExpectedValues()
+        {
+            // Call
+            var converter = new MessageTypeConverter();
+
+            // Assert
+            Assert.That(converter, Is.InstanceOf<IValueConverter>());
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetNonMessageTypes))]
+        public void Convert_WithInvalidValueTypes_ThrowsNotSupportedException(object unsupportedValue)
+        {
+            // Setup
+            var converter = new MessageTypeConverter();
+
+            // Call 
+            TestDelegate call = () => converter.Convert(unsupportedValue, typeof(Brush), null, null);
+
+            // Assert
+            string expectedMessage = $"Conversion from {unsupportedValue?.GetType().Name} is not supported.";
+            Assert.That(call, Throws.Exception.InstanceOf<NotSupportedException>()
+                                    .And.Message.EqualTo(expectedMessage));
+        }
+
+        [Test]
+        public void Convert_WithInvalidTargetType_ThrowsNotSupportedException()
+        {
+            // Setup
+            var random = new Random(21);
+            var converter = new MessageTypeConverter();
+
+            Type unsupportedType = typeof(int);
+
+            // Call
+            TestDelegate call = () => converter.Convert(random.NextEnum<MessageType>(), unsupportedType, null, null);
+
+            // Assert
+            string expectedMessage = $"Conversion to {unsupportedType.Name} is not supported.";
+            Assert.That(call, Throws.Exception.InstanceOf<NotSupportedException>()
+                                    .And.Message.EqualTo(expectedMessage));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetConvertCases))]
+        public void Convert_WithValidMessageTypeAndTargetValueObject_ReturnsExpectedValue(
+            MessageType valueToConvert, Brush expectedValue)
+        {
+            // Setup
+            var converter = new MessageTypeConverter();
+
+            // Call
+            object convertedValue = converter.Convert(valueToConvert, typeof(object), null, null);
+
+            // Assert
+            Assert.That(convertedValue, Is.InstanceOf<Brush>());
+            Assert.That(convertedValue, Is.EqualTo(expectedValue));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetConvertCases))]
+        public void Convert_WithValidMessageTypeAndTargetValueBrush_ReturnsExpectedValue(
+            MessageType valueToConvert, Brush expectedValue)
+        {
+            // Setup
+            var converter = new MessageTypeConverter();
+
+            // Call
+            object convertedValue = converter.Convert(valueToConvert, typeof(Brush), null, null);
+
+            // Assert
+            Assert.That(convertedValue, Is.InstanceOf<Brush>());
+            Assert.That(convertedValue, Is.EqualTo(expectedValue));
+        }
+
+        [Test]
+        public void Convert_WithInvalidMessageType_ThrowsNotSupportedException()
+        {
+            // Setup
+            const MessageType invalidMessageType = (MessageType) 1001;
+            
+            var converter = new MessageTypeConverter();
+
+            // Call
+            TestDelegate call = () => converter.Convert(invalidMessageType, typeof(object), null, null);
+
+            // Assert
+            string expectedMessage = $"The value of argument 'value' ({(int)invalidMessageType}) is invalid for Enum type '{typeof(MessageType).Name}'." +
+                                     $"{Environment.NewLine}Parameter name: value";
+            Assert.That(call, Throws.Exception.InstanceOf<InvalidEnumArgumentException>()
+                                    .And.Message.EqualTo(expectedMessage));
+        }
+
+        [Test]
+        public void ConvertBack_Always_ThrowsNotSupportedException()
+        {
+            // Setup
+            var converter = new MessageTypeConverter();
+
+            // Call
+            TestDelegate call = () => converter.ConvertBack(null, null, null, null);
+
+            // Assert
+            Assert.That(call, Throws.Exception.TypeOf<NotSupportedException>()
+                                    .With.Message.EqualTo("ConvertBack operation is not supported."));
+        }
+
+        private static IEnumerable<TestCaseData> GetConvertCases()
+        {
+            yield return new TestCaseData(MessageType.Info, Brushes.CornflowerBlue);
+            yield return new TestCaseData(MessageType.Error, Brushes.Crimson);
+            yield return new TestCaseData(MessageType.Warning, Brushes.Gold);
+        }
+
+        private static IEnumerable<TestCaseData> GetNonMessageTypes()
+        {
+            yield return new TestCaseData(null);
+            yield return new TestCaseData(new object());
+        }
+    }
+}
