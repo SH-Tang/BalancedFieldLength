11459e720acef29d62aa991b7fba9fc2287a0d86
Dennis Tang <dennis.sht@gmail.com>
2021-01-17 10:32:55 +0100

    Added proof of concept for validation rules for doubles

---
+++ BalancedFieldLength/Core/src/Core.Common.Data/DataModel/ValidationRules/DoubleRule.cs
@@ -0,0 +1,42 @@
+﻿namespace Core.Common.Data.DataModel.ValidationRules
+{
+    public abstract class DoubleRule : ComparableValidationRule<double>
+    {
+        protected DoubleRule(double value, string parameterName, double lowerLimit, double upperLimit)
+            : base(value, parameterName, lowerLimit, upperLimit) {}
+
+        public static DoubleRule GreaterThan(double value, string parameterName, double lowerLimit)
+        {
+            return new GreaterThanRule(value, parameterName, lowerLimit);
+        }
+
+        public static DoubleRule GreaterOrEqualTo(double value, string parameterName, double lowerLimit)
+        {
+            return new GreaterOrEqualToRule(value, parameterName, lowerLimit);
+        }
+
+        private class GreaterOrEqualToRule : DoubleRule
+        {
+            public GreaterOrEqualToRule(double value, string parameterName,
+                                        double lowerLimit)
+                : base(value, parameterName, lowerLimit, double.MaxValue) {}
+
+            protected override string GetInvalidResultMessage()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        private class GreaterThanRule : DoubleRule
+        {
+            public GreaterThanRule(double value, string parameterName,
+                                   double lowerLimit)
+                : base(value, parameterName, lowerLimit + double.Epsilon, double.MaxValue) {}
+
+            protected override string GetInvalidResultMessage()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
