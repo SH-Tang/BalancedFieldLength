31fe956654ee854ce15e688fce68c6f0e516d2df
Dennis Tang <karabasf@gmail.com>
2021-01-27 16:33:45 +0100

    Added validation rule providers for the general simulation settings data.

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength/ValidationRuleProviders/GeneralSimulationSettingsDataRuleProvider.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Application.BalancedFieldLength.Data;
+using Core.Common.Data.DataModel;
+using Core.Common.Data.DataModel.ValidationRules;
+
+namespace Application.BalancedFieldLength.ValidationRuleProviders
+{
+    /// <summary>
+    /// A <see cref="IDataModelRuleProvider"/> to provide validation rules for instances of <see cref="GeneralSimulationSettingsData"/>.
+    /// </summary>
+    public class GeneralSimulationSettingsDataRuleProvider : IDataModelRuleProvider
+    {
+        private readonly GeneralSimulationSettingsData data;
+
+        /// <summary>
+        /// Creates an instance of <see cref="EngineDataRuleProvider"/>.
+        /// </summary>
+        /// <param name="data">The <see cref="EngineData"/> to create the provider for.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="data"/> is <c>null</c>.</exception>
+        public GeneralSimulationSettingsDataRuleProvider(GeneralSimulationSettingsData data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            this.data = data;
+        }
+
+        public IEnumerable<IDataModelValidationRule> GetDataModelValidationRules()
+        {
+            IEnumerable<IDataModelValidationRule> doubleParameterRules =
+                CreateDoubleParameterValidationRules("Density", data.Density)
+                    .Concat(CreateDoubleParameterValidationRules("Gravitational acceleration", data.GravitationalAcceleration))
+                    .Concat(CreateDoubleParameterValidationRules("Time step", data.TimeStep));
+            foreach (IDataModelValidationRule rule in doubleParameterRules)
+            {
+                yield return rule;
+            }
+
+            yield return new ComparableParameterGreaterThanRule<int>("Maximum number of iterations", data.MaximumNrOfIterations, 0);
+            yield return new ComparableParameterGreaterThanRule<int>("End failure velocity", data.EndFailureVelocity, 0);
+        }
+
+        private static IEnumerable<IDataModelValidationRule> CreateDoubleParameterValidationRules(string parameterName, double value)
+        {
+            yield return new DoubleParameterConcreteValueRule(parameterName, value);
+            yield return new ComparableParameterGreaterThanRule<double>(parameterName, value, 0);
+        }
+    }
+}
---
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.Test/ValidationRuleProviders/GeneralSettingsDataRuleProviderTest.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Linq;
+using Application.BalancedFieldLength.Data;
+using Application.BalancedFieldLength.ValidationRuleProviders;
+using Core.Common.Data.DataModel;
+using Core.Common.Data.DataModel.ValidationRules;
+using NUnit.Framework;
+
+namespace Application.BalancedFieldLength.Test.ValidationRuleProviders
+{
+    [TestFixture]
+    public class GeneralSettingsDataRuleProviderTest
+    {
+        [Test]
+        public void Constructor_DataNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => new GeneralSimulationSettingsDataRuleProvider(null);
+
+            // Assert
+            Assert.That(call, Throws.ArgumentNullException
+                                    .With.Property(nameof(ArgumentNullException.ParamName))
+                                    .EqualTo("data"));
+        }
+
+        [Test]
+        public void Constructor_WithArguments_ExpectedValues()
+        {
+            // Setup
+            var data = new GeneralSimulationSettingsData();
+
+            // Call
+            var provider = new GeneralSimulationSettingsDataRuleProvider(data);
+
+            // Assert
+            Assert.That(provider, Is.InstanceOf<IDataModelRuleProvider>());
+        }
+
+        [Test]
+        public void GetDataModelValidationRules_Always_ReturnsExpectedValidationRules()
+        {
+            // Setup
+            var data = new GeneralSimulationSettingsData();
+            var provider = new GeneralSimulationSettingsDataRuleProvider(data);
+
+            // Call 
+            IDataModelValidationRule[] rules = provider.GetDataModelValidationRules().ToArray();
+
+            // Assert
+            Assert.That(rules, Has.Length.EqualTo(8));
+            Assert.That(rules[0], Is.TypeOf<DoubleParameterConcreteValueRule>());
+            Assert.That(rules[1], Is.TypeOf<ComparableParameterGreaterThanRule<double>>());
+            Assert.That(rules[2], Is.TypeOf<DoubleParameterConcreteValueRule>());
+            Assert.That(rules[3], Is.TypeOf<ComparableParameterGreaterThanRule<double>>());
+            Assert.That(rules[4], Is.TypeOf<DoubleParameterConcreteValueRule>());
+            Assert.That(rules[5], Is.TypeOf<ComparableParameterGreaterThanRule<double>>());
+            
+            Assert.That(rules[6], Is.TypeOf<ComparableParameterGreaterThanRule<int>>());
+            Assert.That(rules[7], Is.TypeOf<ComparableParameterGreaterThanRule<int>>());
+        }
+    }
+}
