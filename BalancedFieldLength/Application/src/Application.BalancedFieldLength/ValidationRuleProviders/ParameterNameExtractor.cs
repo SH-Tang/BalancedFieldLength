c6541fac6e65f2ccedaf62a9b4c5a545a8eb6e19
Dennis Tang <karabasf@gmail.com>
2021-02-10 16:48:15 +0100

    Introduced helper class to extract parameter names from string values.

---
+++ BalancedFieldLength/Application/src/Application.BalancedFieldLength/ValidationRuleProviders/ParameterNameExtractor.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace Application.BalancedFieldLength.ValidationRuleProviders
+{
+    /// <summary>
+    /// Helper class to extract parameter names.
+    /// </summary>
+    public static class ParameterNameExtractor
+    {
+        private static readonly char[] splitOptions = new[]
+        {
+            '[',
+            ']'
+        };
+
+        /// <summary>
+        /// Extract a parameter name from its string argument.
+        /// </summary>
+        /// <param name="value">The string to extract a parameter name from.</param>
+        /// <returns>A parameter name.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is empty.</exception>
+        public static string ExtractParameterName(string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            string[] substrings = value.Split(splitOptions);
+            return substrings[0].TrimEnd();
+        }
+    }
+}
---
+++ BalancedFieldLength/Application/test/Application.BalancedFieldLength.Test/ValidationRuleProviders/ParameterNameExtractorTest.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (C) 2018 Dennis Tang. All rights reserved.
+//
+// This file is part of Balanced Field Length.
+//
+// Balanced Field Length is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using Application.BalancedFieldLength.ValidationRuleProviders;
+using NUnit.Framework;
+
+namespace Application.BalancedFieldLength.Test.ValidationRuleProviders
+{
+    [TestFixture]
+    public class ParameterNameExtractorTest
+    {
+        [Test]
+        public void ExtractParameterName_ValueNull_ThrowsArgumentNullException()
+        {
+            // Call
+            TestDelegate call = () => ParameterNameExtractor.ExtractParameterName(null);
+
+            // Assert
+            Assert.That(call, Throws.ArgumentNullException
+                                    .With.Property(nameof(ArgumentNullException.ParamName))
+                                    .EqualTo("value"));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetValues))]
+        public void ExtractParameterName_WithValues_ReturnsExpectedValues(string value, string expectedValue)
+        {
+            // Call 
+            string extractedName = ParameterNameExtractor.ExtractParameterName(value);
+
+            // Assert
+            Assert.That(extractedName, Is.EqualTo(expectedValue));
+        }
+
+        private static IEnumerable<TestCaseData> GetValues()
+        {
+            yield return new TestCaseData("ParameterName", "ParameterName");
+            yield return new TestCaseData("Parameter Name     ", "Parameter Name");
+            yield return new TestCaseData("    Parameter Name", "    Parameter Name");
+            yield return new TestCaseData(string.Empty, string.Empty);
+            yield return new TestCaseData("    ", string.Empty);
+            yield return new TestCaseData("ParameterName [unit]", "ParameterName");
+            yield return new TestCaseData("ParameterName [unit", "ParameterName");
+            yield return new TestCaseData("ParameterName unit]", "ParameterName unit");
+            yield return new TestCaseData("Parameter Name [unit]", "Parameter Name");
+        }
+    }
+}
